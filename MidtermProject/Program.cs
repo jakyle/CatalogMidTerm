cecb8947550a58df8fcfc36833604cc3ca0db4bf
chandrafloyd <chandra_floyd@yahoo.com>
2018-02-08 11:21:30 -0500

    first commit

---
+++ MidtermProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//adding "using MidtermProject." so that we can access the new interfaces we created in the folder hierarchy
+using MidtermProject.Interfaces;
+using MidtermProject.Enums;
+using MidtermProject.Classes;
+
+namespace MidtermProject
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+       
+        {
+            try
+            {
+                //app goes here
+
+                
+
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Dead end. You're done here. Stick a fork in it.");
+            }
+
+        }
+    }
+}
